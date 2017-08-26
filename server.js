ead25a5a1a06ef026c1f3058748864f952ce3c86
Blake Desktop <bhhuynh@ucsd.edu>
2017-08-26 00:54:23 -0700

    port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
-
+var port = process.env.PORT || 3000;
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -200,7 +200,7 @@ app.get("/saved_article",function(req,res){
 })
 
 
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+//Express listening to port
+app.listen(port, function() {
+    console.log("express listening to", port)
 });
